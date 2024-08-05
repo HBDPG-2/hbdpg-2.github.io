b067256e36af80856675ad916b5a881f28ee87d8
Piotr-Kniaz <piotr.kniaz@ya.ru>
2024-08-05 22:40:14 +0300

    UI update: add checkboxes "Show password" and scripts

--- scripts/script.js
+++ scripts/script.js
@@ -3,33 +3,44 @@ document.addEventListener('DOMContentLoaded', function () {
     const form2 = document.getElementById('passphrase2Form');
     const passphrase1Input = document.getElementById('passphrase1Input');
     const passphrase2Input = document.getElementById('passphrase2Input');
+    const showPassphrase1Checkbox = document.getElementById('showPassphrase1Checkbox');
+    const showPassphrase2Checkbox = document.getElementById('showPassphrase2Checkbox');
+    const showPasswordCheckbox = document.getElementById('showPasswordCheckbox');
     const nextButton = document.getElementById('nextButton');
     const confirmButton = document.getElementById('confirmButton');
     const generateButton = document.getElementById('generateButton');
     const copyButton = document.getElementById('copyButton');
+    const clearButton = document.getElementById('clearButton');
     const result = document.getElementById('result');
 
     form1.removeAttribute('action');
     form2.removeAttribute('action');
 
-    form1.addEventListener('submit', function (event) {
+    form1.addEventListener('submit', function(event) {
         event.preventDefault();
 
+        passphrase1Input.type = 'password';
         passphrase1Input.setAttribute('disabled', 'disabled');
+        showPassphrase1Checkbox.checked = false;
+        showPassphrase1Checkbox.setAttribute('disabled', 'disabled');
         nextButton.setAttribute('disabled', 'disabled');
         
         passphrase2Input.removeAttribute('disabled');
+        showPassphrase2Checkbox.removeAttribute('disabled');
         confirmButton.removeAttribute('disabled');
 
         passphrase2Input.type = 'password';
         passphrase2Input.focus();
     });
 
-    form2.addEventListener('submit', function (event) {
+    form2.addEventListener('submit', function(event) {
         event.preventDefault();
 
         if (passphrase1Input.value != passphrase2Input.value) {
+            passphrase2Input.type = 'password';
             passphrase2Input.setAttribute('disabled', 'disabled');
+            showPassphrase2Checkbox.checked = false;
+            showPassphrase2Checkbox.setAttribute('disabled', 'disabled');
             confirmButton.setAttribute('disabled', 'disabled');
 
             generateButton.removeAttribute('disabled');
@@ -38,6 +49,46 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Passphrases must be different!');
             passphrase2Input.focus();
         }
+    });
+
+    showPassphrase1Checkbox.addEventListener('click', function() {
+        passphrase1Input.focus();
+
+        if (passphrase1Input.type === 'password') {
+            passphrase1Input.type = 'text';
+        } else {
+            passphrase1Input.type = 'password';
+        }
+    });
+
+    showPassphrase2Checkbox.addEventListener('click', function() {
+        passphrase2Input.focus();
+
+        if (passphrase2Input.type === 'password') {
+            passphrase2Input.type = 'text';
+        } else {
+            passphrase2Input.type = 'password';
+        }
+    });
+
+    showPasswordCheckbox.addEventListener('click', function() {
+        if (result.type === 'password') {
+            result.type = 'text';
+        } else {
+            result.type = 'password';
+        }
+    });
+
+    copyButton.addEventListener('click', function() {
+        navigator.clipboard.writeText(result.value);
+
+        clearButton.focus();
+    });
+
+    clearButton.addEventListener('click', function() {
+        navigator.clipboard.writeText('');
+
+        document.location.reload();
     });
 });
 
@@ -51,18 +102,25 @@ function hashnow() {
             salt: sha512(passphrase1Input.value),
             secret: passphrase2Input.value,
             ad: sha512(passphrase1Input.value),
-            time: 48,
-            mem: 256000,
+            // time: 48,
+            // mem: 256000,
+            // hashLen: 32,
+            // parallelism: 24,
+            // type: argon2.ArgonType.Argon2id
+            time: 1,
+            mem: 1024,
             hashLen: 32,
-            parallelism: 24,
+            parallelism: 1,
             type: argon2.ArgonType.Argon2id
         })
         .then(hash => {
         //    document.querySelector('pre').innerText =
         //        `Encoded: ${hash.encoded}\n` +
         //        `Hex: ${hash.hashHex}\n`;
-            result.setAttribute('value', hash.hashHex);
+            result.value = hash.hashHex;
+            showPasswordCheckbox.removeAttribute('disabled');
             copyButton.removeAttribute('disabled');
+            copyButton.focus();
         })
         .catch(e => console.error('Error: ', e));
 }
