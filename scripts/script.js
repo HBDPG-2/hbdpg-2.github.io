8754b96f0af149d4e7aaf932d4890ca45ac83983
Piotr-Kniaz <piotr.kniaz@ya.ru>
2024-08-07 02:58:09 +0300

    UI update: add beta logo, change loading animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const form1 = document.getElementById('passphrase1Form');
     const form2 = document.getElementById('passphrase2Form');
     const passphrase1Input = document.getElementById('passphrase1Input');
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const showPassphrase1Checkbox = document.getElementById('showPassphrase1Checkbox');
     const showPassphrase2Checkbox = document.getElementById('showPassphrase2Checkbox');
     const showPasswordCheckbox = document.getElementById('showPasswordCheckbox');
+    const showPassphrase1CheckboxLabel = document.getElementById('showPassphrase1CheckboxLabel');
+    const showPassphrase2CheckboxLabel = document.getElementById('showPassphrase2CheckboxLabel');
+    const showPasswordCheckboxLabel = document.getElementById('showPasswordCheckboxLabel');
     const nextButton = document.getElementById('nextButton');
     const confirmButton = document.getElementById('confirmButton');
     const generateButton = document.getElementById('generateButton');
@@ -15,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const copyNote = document.getElementById('copyNote');
     const clearNote = document.getElementById('clearNote');
     const result = document.getElementById('result');
+    
 
     form1.removeAttribute('action');
     form2.removeAttribute('action');
@@ -26,10 +30,12 @@ document.addEventListener('DOMContentLoaded', function () {
         passphrase1Input.setAttribute('disabled', 'disabled');
         showPassphrase1Checkbox.checked = false;
         showPassphrase1Checkbox.setAttribute('disabled', 'disabled');
+        showPassphrase1CheckboxLabel.style.color = '#202020';
         nextButton.setAttribute('disabled', 'disabled');
         
         passphrase2Input.removeAttribute('disabled');
         showPassphrase2Checkbox.removeAttribute('disabled');
+        showPassphrase2CheckboxLabel.style.color = 'white';
         confirmButton.removeAttribute('disabled');
 
         passphrase2Input.type = 'password';
@@ -44,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
             passphrase2Input.setAttribute('disabled', 'disabled');
             showPassphrase2Checkbox.checked = false;
             showPassphrase2Checkbox.setAttribute('disabled', 'disabled');
+            showPassphrase2CheckboxLabel.style.color = '#202020';
             confirmButton.setAttribute('disabled', 'disabled');
 
             generateButton.removeAttribute('disabled');
@@ -84,10 +91,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    generateButton.addEventListener('click', function() {
+        generateButton.setAttribute('disabled', 'disabled');
+        generateButton.innerHTML = '<img id="loading" src="images/loading.webp" alt="Loaning">';
+        // generateButton.style.boxShadow = '0';
+        generateNote.style.visibility = 'visible';
+
+        setTimeout(hashnow, 500);
+    });
+
     copyButton.addEventListener('click', function() {
         navigator.clipboard.writeText(result.value);
         this.innerHTML = 'Copied';
-        // copyNote.style.visibility = 'visible';
         clearButton.focus();
     });
 
@@ -99,36 +114,35 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function hashnow() {
-    generateButton.setAttribute('disabled', 'disabled');
-    generateButton.innerHTML = '<img id="loading" src="images/loading.png" alt="Loaning">';
-    generateNote.style.visibility = 'visible';
-    // generate button loading...
-
     argon2
         .hash({
             pass: passphrase1Input.value,
             salt: sha512(passphrase1Input.value),
             secret: passphrase2Input.value,
             ad: sha512(passphrase1Input.value),
-            // time: 48,
-            // mem: 256000,
-            // hashLen: 32,
-            // parallelism: 24,
-            // type: argon2.ArgonType.Argon2id
-            time: 1,
-            mem: 1024,
+            time: 48,
+            mem: 256000,
             hashLen: 32,
-            parallelism: 1,
+            parallelism: 24,
             type: argon2.ArgonType.Argon2id
+            // time: 1,
+            // mem: 1024,
+            // hashLen: 32,
+            // parallelism: 1,
+            // type: argon2.ArgonType.Argon2id
         })
         .then(hash => {
         //    document.querySelector('pre').innerText =
         //        `Encoded: ${hash.encoded}\n` +
         //        `Hex: ${hash.hashHex}\n`;
+            result.value = hash.hashHex;
+            result.removeAttribute('disabled');
+            result.style.boxShadow = '0px 0px 25px rgba(179, 71, 230, 0.8)';
+
             generateButton.innerHTML = '<b>Generated</b>';
             generateNote.style.visibility = 'hidden';
-            result.value = hash.hashHex;
             showPasswordCheckbox.removeAttribute('disabled');
+            showPasswordCheckboxLabel.style.color = 'white';
             copyButton.removeAttribute('disabled');
             copyButton.focus();
         })
